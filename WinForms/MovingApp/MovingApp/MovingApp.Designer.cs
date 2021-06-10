fabec4e54a5299dc90c520de0e4f6c59ed2ea16f
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-06-10 15:39:29 -0700

    WinForm App

---
+++ WinForms/MovingApp/MovingApp/MovingApp.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MovingApp
+{
+    partial class MovingAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBxX = new System.Windows.Forms.TextBox();
+            this.txtBxY = new System.Windows.Forms.TextBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.lblErrorMsg = new System.Windows.Forms.Label();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnPurple = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Change Form Location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "New X Coordinate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 182);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "New Y Coordinate:";
+            // 
+            // txtBxX
+            // 
+            this.txtBxX.Location = new System.Drawing.Point(238, 125);
+            this.txtBxX.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBxX.Name = "txtBxX";
+            this.txtBxX.Size = new System.Drawing.Size(98, 30);
+            this.txtBxX.TabIndex = 3;
+            this.txtBxX.TextChanged += new System.EventHandler(this.txtBxX_TextChanged);
+            // 
+            // txtBxY
+            // 
+            this.txtBxY.Location = new System.Drawing.Point(238, 182);
+            this.txtBxY.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBxY.Name = "txtBxY";
+            this.txtBxY.Size = new System.Drawing.Size(98, 30);
+            this.txtBxY.TabIndex = 4;
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(399, 150);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(101, 34);
+            this.btnMove.TabIndex = 5;
+            this.btnMove.Text = "Move Form";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // lblErrorMsg
+            // 
+            this.lblErrorMsg.AutoSize = true;
+            this.lblErrorMsg.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorMsg.Location = new System.Drawing.Point(33, 327);
+            this.lblErrorMsg.Name = "lblErrorMsg";
+            this.lblErrorMsg.Size = new System.Drawing.Size(0, 25);
+            this.lblErrorMsg.TabIndex = 6;
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(81, 293);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(111, 37);
+            this.btnGreen.TabIndex = 7;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(198, 293);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(124, 37);
+            this.btnRed.TabIndex = 8;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 235);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(211, 36);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Change Color";
+            // 
+            // btnPurple
+            // 
+            this.btnPurple.Location = new System.Drawing.Point(340, 293);
+            this.btnPurple.Name = "btnPurple";
+            this.btnPurple.Size = new System.Drawing.Size(124, 37);
+            this.btnPurple.TabIndex = 10;
+            this.btnPurple.Text = "Purple";
+            this.btnPurple.UseVisualStyleBackColor = true;
+            this.btnPurple.Click += new System.EventHandler(this.btnPurple_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(189, 373);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(164, 42);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // MovingAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(582, 503);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPurple);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.lblErrorMsg);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.txtBxY);
+            this.Controls.Add(this.txtBxX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Location = new System.Drawing.Point(30, 30);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MovingAppForm";
+            this.Text = "Moving App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBxX;
+        private System.Windows.Forms.TextBox txtBxY;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Label lblErrorMsg;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnPurple;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ WinForms/MovingApp/MovingApp/MovingApp.cs
@@ -0,0 +1,78 @@
+﻿/* MovingApp.cs
+ * This programs asks the user to 
+ * enter two coordinates and then
+ * moves the form to that location
+ * on the screen.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingApp
+{
+    public partial class MovingAppForm : Form
+    {
+        public MovingAppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            int x,
+                y;
+            while (int.TryParse(txtBxX.Text, out x) == false)
+            {
+                lblErrorMsg.Text = "Value entered must be numeric";
+                txtBxX.Text = "0";
+                txtBxX.Focus();
+            }
+
+            while (int.TryParse(txtBxY.Text, out y) == false)
+            {
+                lblErrorMsg.Text = "Value entered must be numeric";
+                txtBxY.Text = "0";
+                txtBxY.Focus();
+            }
+
+            if (txtBxX.Text != "0")
+                if (txtBxY.Text != "0")
+                    Location = new Point(x, y);
+        }
+
+        private void txtBxX_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Green;
+            MessageBox.Show("Color selection was: " + BackColor);
+        }
+
+        private void btnPurple_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Purple;
+            MessageBox.Show("Color selection was: " + BackColor);
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.Red;
+            MessageBox.Show("Color selection was: " + BackColor);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+        }
+    }
+}
