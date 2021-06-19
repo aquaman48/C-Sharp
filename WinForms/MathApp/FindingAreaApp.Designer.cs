5033872e78c294bbbab448cce4c60dadc97ccf65
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-06-18 17:06:54 -0700

    Math App
    In progress

---
+++ WinForms/MathApp/FindingAreaApp.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace MathApp
+{
+    partial class AreaCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AreaCalculator));
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnCylinder = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.btnCircle.Location = new System.Drawing.Point(26, 12);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(135, 40);
+            this.btnCircle.TabIndex = 0;
+            this.btnCircle.Text = "Circle";
+            this.btnCircle.UseVisualStyleBackColor = true;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnCylinder
+            // 
+            this.btnCylinder.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.btnCylinder.Location = new System.Drawing.Point(228, 12);
+            this.btnCylinder.Name = "btnCylinder";
+            this.btnCylinder.Size = new System.Drawing.Size(135, 40);
+            this.btnCylinder.TabIndex = 1;
+            this.btnCylinder.Text = "Cylinder";
+            this.btnCylinder.UseVisualStyleBackColor = true;
+            this.btnCylinder.Click += new System.EventHandler(this.btnCylinder_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.btnRectangle.Location = new System.Drawing.Point(431, 12);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(135, 40);
+            this.btnRectangle.TabIndex = 2;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.BackColor = System.Drawing.Color.White;
+            this.lblInput.Location = new System.Drawing.Point(456, 158);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(49, 22);
+            this.lblInput.TabIndex = 7;
+            this.lblInput.Text = "Input";
+            this.lblInput.Visible = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(379, 309);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 28);
+            this.lblResult.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(383, 222);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 30);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Visible = false;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(264, 328);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(143, 57);
+            this.btnResult.TabIndex = 11;
+            this.btnResult.Text = "Compute";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Visible = false;
+            this.btnResult.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AreaCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(598, 421);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnRectangle);
+            this.Controls.Add(this.btnCylinder);
+            this.Controls.Add(this.btnCircle);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AreaCalculator";
+            this.Text = "Surface Area Calculator";
+            this.Load += new System.EventHandler(this.AreaCalculator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnCylinder;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnResult;
+    }
+}
+
---
+++ WinForms/MathApp/FindingAreaApp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+
+
+namespace MathApp
+{
+    public partial class AreaCalculator : Form
+    {
+        
+        public AreaCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void AreaCalculator_Load(object sender, EventArgs e)
+        {
+            string title = "IMPORTANT PLEASE READ!!!!";
+            string message = "This application enables you to compute different measurements" +
+                             " for three figure types." +
+                            " You will be asked to select a figure and then asked " +
+                            "to enter it\'s unique measurement(s). " +
+                            " The area will be calculated and displayed.";
+
+            MessageBox.Show(message, title);
+
+                
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            lblInput.Visible = true;
+            
+            
+            lblResult.Visible = true;
+            textBox1.Visible = true;
+            btnResult.Visible = true;
+           
+
+
+        }
+
+        private void btnCylinder_Click(object sender, EventArgs e)
+        {
+            lblInput.Visible = true;
+
+            lblResult.Visible = true;
+        }
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            lblInput.Visible = true;
+
+            lblResult.Visible = true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string q = textBox1.Text;
+
+            lblResult.Text = q;
+        }
+    }
+        
+    }
+
